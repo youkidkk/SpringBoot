ca3007d65a1bee33a9506c7453523f34d9826f5c
youkidkk <youkidkk80@gmail.com>
2017-08-09 06:43:14 +0900

    TaskSample を追加

---
+++ SpringBootTaskSample/src/main/java/springboot/task/sample/ScheduleTaskSample.java
@@ -0,0 +1,34 @@
+package springboot.task.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.text.MessageFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@SuppressWarnings("javadoc")
+@SpringBootApplication
+@EnableScheduling
+@ImportResource("applicationContext.xml")
+public class ScheduleTaskSample {
+
+    public static final DateTimeFormatter DATETIME_FORMAT = DateTimeFormatter
+            .ofPattern("yyyy-MM-dd HH:mm:ss:SSS");
+
+    // initialDelay で初回のディレイを指定
+    // fixedRate で間隔を指定
+    @Scheduled(initialDelay = 3000, fixedRate = 1000)
+    public void task() {
+        System.out.println(MessageFormat.format("{0} Schedule task running...",
+                LocalDateTime.now().format(DATETIME_FORMAT)));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleTaskSample.class, args);
+    }
+
+}
