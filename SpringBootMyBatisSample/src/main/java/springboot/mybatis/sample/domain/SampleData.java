a6517458a577470602597b580421f67819bc4b14
youkidkk <youkidkk80@gmail.com>
2017-08-10 21:36:11 +0900

    SpringBootMyBatisSample を追加

---
+++ SpringBootMyBatisSample/src/main/java/springboot/mybatis/sample/domain/SampleData.java
@@ -0,0 +1,13 @@
+package springboot.mybatis.sample.domain;
+
+@SuppressWarnings("javadoc")
+@lombok.Data
+public class SampleData {
+
+    private int id;
+
+    private String name;
+
+    private String value;
+
+}
