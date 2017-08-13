858ab6e53b9f443b5846ad22ffd04eae374ef91a
youkidkk <youkidkk80@gmail.com>
2017-08-13 10:10:58 +0900

    warファイル用の設定を削除

--- SpringBootMvcSample/src/main/java/springboot/sample/SpringBootSampleApplication.java
+++ SpringBootMvcSample/src/main/java/springboot/sample/SpringBootSampleApplication.java
@@ -2,13 +2,12 @@ package springboot.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * SpringBootサンプル・アプリケーション
  */
 @SpringBootApplication
-public class SpringBootSampleApplication extends SpringBootServletInitializer {
+public class SpringBootSampleApplication {
 
     /**
      * メインメソッド
